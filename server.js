3f5c7e9a721af805961e70af6f8152231ee1c31f
jypara-git <zhyparaabdieva@gmail.com>
2021-04-08 23:20:58 -0700

    requests to serve html pages

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -74,7 +75,7 @@ function validateAnimal(animal) {
     if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
         return false;
     }
-    return false;
+    return true;
 }
 app.get('/api/animals/', (req, res) => {
     let results = animals;
@@ -102,6 +103,18 @@ app.get('/api/animals/:id', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+  });
+  app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+      res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+  app.get('/zookeepers', (req, res) => {
+      res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, './public/index.html'));
   });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
